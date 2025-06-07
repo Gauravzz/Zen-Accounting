a345680fcea1f329f37117631377c61f9b49e53b
Gauravzz <gaurav.ax8@gmail.com>
2025-06-07 21:34:53 +0530

    commit with dist including frontend and backend

--- frontend/components/Home.tsx
+++ frontend/components/Home.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom";
 
 // Define the structure of account object to type script
 type Account = {
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
